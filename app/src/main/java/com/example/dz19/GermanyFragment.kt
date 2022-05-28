f9dec5d3907a5a40a3b88818d13838c6f9abb41e
Roman <romanbratishkingg@gmail.com>
2022-05-28 21:32:22 +0300

    created fragments for dz19

---
+++ app/src/main/java/com/example/dz19/GermanyFragment.kt
@@ -0,0 +1,23 @@
+package com.example.dz19
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GermanyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_germany, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = GermanyFragment()
+    }
+}
--- app/src/main/java/com/example/dz19/MainActivity.kt
+++ app/src/main/java/com/example/dz19/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.dz19
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dz19.databinding.ActivityMainBinding
@@ -11,33 +10,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnToFrance.setOnClickListener {
-            val intent = Intent(this@MainActivity, FranceActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToGermany.setOnClickListener {
-            val intent = Intent(this@MainActivity, GermanyActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToBelgium.setOnClickListener {
-            val intent = Intent(this@MainActivity, BelgiumActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToLatvia.setOnClickListener {
-            val intent = Intent(this@MainActivity, LatviaActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToThailand.setOnClickListener {
-            val intent = Intent(this@MainActivity, ThailandActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToMadagascar.setOnClickListener {
-            val intent = Intent(this@MainActivity, MadagascarActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnToSweden.setOnClickListener {
-            val intent = Intent(this@MainActivity, SwedenActivity::class.java)
-            startActivity(intent)
-        }
+        binding.btnToFrance.setOnClickListener { startFranceFragment() }
+        binding.btnToGermany.setOnClickListener { startGermanyFragment() }
+        binding.btnToBelgium.setOnClickListener { startBelgiumFragment() }
+        binding.btnToLatvia.setOnClickListener { startLatviaFragment() }
+        binding.btnToThailand.setOnClickListener { startThailandFragment() }
+        binding.btnToMadagascar.setOnClickListener { startMadagascarFragment() }
+        binding.btnToSweden.setOnClickListener { startSwedenFragment() }
     }
+
+private fun startFranceFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, FranceFragment.newInstance()).commit()
+}
+private fun startGermanyFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, GermanyFragment.newInstance()).commit()
+}
+private fun startBelgiumFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, BelgiumFragment.newInstance()).commit()
+}
+private fun startLatviaFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, LatviaFragment.newInstance()).commit()
+}
+private fun startThailandFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, ThailandFragment.newInstance()).commit()
+}
+private fun startMadagascarFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, MadagascarFragment.newInstance()).commit()
+}
+private fun startSwedenFragment() {
+    supportFragmentManager.beginTransaction().replace(R.id.conteiner, SwedenFragment.newInstance()).commit()
+}
 }
