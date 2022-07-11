3f3aac429ca5e50493fdd5cfed1bba1a391a14c3
Roman <romanbratishkingg@gmail.com>
2022-07-11 03:34:37 +0300

    created viewModel and viewModelScope

--- app/src/main/java/com/example/zadanie23/MainActivity.kt
+++ app/src/main/java/com/example/zadanie23/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.zadanie23
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.lifecycle.viewModelScope
+import com.example.zadanie23.adapter.PeopleAdapter
 import com.example.zadanie23.databinding.ActivityMainBinding
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var userList: MutableList<User> = mutableListOf()
+    private lateinit var btnAdd:Button
+    private lateinit var btnUpdate:Button
+    private lateinit var etInpName: EditText
+    private lateinit var etInpSurname: EditText
+    private lateinit var etInpAge: EditText
+
+    private  val userViewModel: PeopleViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.rvList.adapter = com.example.zadanie23.adapter.PeopleAdapter(getUsersData())
+        initialization()
+        updateList()
+        btnAdd.setOnClickListener { createUser()}
+        btnUpdate.setOnClickListener {updateList()}
     }
-    private fun getUsersData() = mutableListOf<User>(
-        User("Вольфганг Амадей", "Моцарт", 35,"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Wolfgang-amadeus-mozart_1.jpg/250px-Wolfgang-amadeus-mozart_1.jpg" ),
-        User("Людвиг Ван", "Бетховен", 56, "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Beethoven.jpg/274px-Beethoven.jpg"),
-        User("Иоганн Себастьян", "Бах", 65, "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Johann_Sebastian_Bach.jpg/260px-Johann_Sebastian_Bach.jpg"),
-        User("Владимир", "Ленин", 53, "https://www.meme-arsenal.com/memes/ede66b8da458aac93faae50f80e4cc7e.jpg"),
-        User("Джозеф", "Байден", 90, "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Joe_Biden_presidential_portrait.jpg/274px-Joe_Biden_presidential_portrait.jpg"),
-        User("Дональд", "Трамп", 50, "https://moe.shikimori.one/system/user_images/original/48969/1263267.jpg"),
-        User("Александр", "Пушкин", 38, "https://upload.wikimedia.org/wikipedia/commons/5/56/Kiprensky_Pushkin.jpg"),
-        User("Александр", "Лука", 67, "https://img5tv.cdnvideo.ru/webp/shared/files/201908/1_955491.jpg"),
-        User("Какой-то", "Мужик", 56, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg/250px-Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg"),
-        User("Владимир", "Путин", 70, "https://cdnn21.img.ria.ru/images/07e6/04/0c/1783086041_0:104:3096:1846_1920x0_80_0_0_a41a71b97d0ec70f22b5812c81f42a6c.jpg"),
-    )
+    private fun createUser(){
+        if ((etInpName.text.toString() != "")&&(etInpSurname.text.toString() != "")&&(etInpAge.text.toString() != "")){
+        userViewModel.usersList.add(User(etInpName.text.toString(), etInpSurname.text.toString(), etInpAge.text.toString().toInt(), "https://moe.shikimori.one/system/user_images/original/48969/1263267.jpg"))
+        }
+    }
+    private fun updateList(){
+        binding.rvList.adapter = PeopleAdapter(userViewModel.usersList)
+    }
+    private fun initialization() {
+        btnAdd = binding.btnAdd
+        btnUpdate = binding.btnUpdate
+        etInpName = binding.etInpName
+        etInpSurname = binding.etImpSurname
+        etInpAge = binding.etInpAge
+    }
 }
